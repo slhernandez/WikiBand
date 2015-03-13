346324a5c14c4568badbe7c49a92adaf6e398d04
Steve Hernandez <slhernandez@gmail.com>
2015-03-12 20:52:57 -0500

    More experimenting with UILabels.

--- WikiBand/ExperimentViewController.m
+++ WikiBand/ExperimentViewController.m
@@ -18,14 +18,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    
-    self.occupationsLabel.text = @"Occupations";
-    self.occupationsValue.text = self.artist.artistOccupation;
+   
+    // Programatically set the UILabel for Occupations Value
+    UILabel *occupationsLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 300.0f, 500.0f)];
+    float textHeight = [self getHeightForText:self.artist.artistOccupation withFont:occupationsLabel.font andWidth:occupationsLabel.frame.size.width];
+    NSLog(@"textHeight is %f", textHeight);
+    occupationsLabel.frame = CGRectMake(0, 0, occupationsLabel.frame.size.width, textHeight);
+    occupationsLabel.text = self.artist.artistOccupation;
+    occupationsLabel.numberOfLines = 0;
+    [self.view addSubview:occupationsLabel];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(float) getHeightForText:(NSString*) text withFont:(UIFont*) font andWidth:(float) width{
+    CGSize constraint = CGSizeMake(width , 20000.0f);
+    CGSize title_size;
+    float totalHeight;
+    
+    SEL selector = @selector(boundingRectWithSize:options:attributes:context:);
+    if ([text respondsToSelector:selector]) {
+        title_size = [text boundingRectWithSize:constraint
+                                        options:NSStringDrawingUsesLineFragmentOrigin
+                                     attributes:@{ NSFontAttributeName : font }
+                                        context:nil].size;
+        
+        totalHeight = ceil(title_size.height);
+    }
+    
+    CGFloat height = MAX(totalHeight, 40.0f);
+    return height;
 }
 
 /*
